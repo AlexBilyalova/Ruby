4e4a05a2b9e6131b53670eee9737cacaa10e3498
sasha <g4092493@gmail.com>
2020-04-27 01:12:35 +0600

    New folder

---
+++ lesson4/4.rb
@@ -0,0 +1,9 @@
+alphabet = ("a".."z").to_a
+
+vowels = "aeiouy".to_s
+vowels_hash = {}
+vowel = 0
+for vowel in vowels
+end
+puts vowels
+vvv
---
+++ lesson4/5.rb
@@ -0,0 +1,16 @@
+puts "Day: "
+dt = gets.chomp.to_i
+puts "Month: "
+mn = gets.chomp.to_i
+puts "Year: "
+yr = gets.chomp.to_i
+
+days_in_a_month = [31,28,31,30,31,30,31,31,30,31,30,31]
+days_in_a_month[0...mn-1].each {|month|}
+
+sum = 0
+for i in 0..mn-2
+  sum+= days_in_a_month[i]
+end
+sum += dt
+puts "#{sum}"
