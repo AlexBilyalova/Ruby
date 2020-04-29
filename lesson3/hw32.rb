7dab35b0662076bbcc1f949e02415df0b0644722
AlexBilyalova <62751265+AlexBilyalova@users.noreply.github.com>
2020-04-29 17:54:33 +0600

    Update hw32.rb

--- lesson3/hw32.rb
+++ lesson3/hw32.rb
@@ -7,12 +7,20 @@ b = gets.chomp.to_f
 puts 'c: '
 c = gets.chomp.to_f
 
+arr=[]
+arr<<[a,b,c]
+
 if a==b && a==c && b==c
   puts 'Ravnostoroniy'
 elsif a==b || a==c || b==c
   puts  'Ravnobedreniy'
-elsif c==(a**2)+(b**2)
+
+max = arr.max
+  puts max
+ 
+arr.each do |x|
+  max == (x**2)+(x**2)
+if max == 0
   puts 'Pryamougolniy'
-else
-  puts 'Raznostoroniy'
+end
 end
